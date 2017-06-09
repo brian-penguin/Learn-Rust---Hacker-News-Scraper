ce3a19f33711c1c1afce961ef67e456be7c1bf70
= <=>
2017-06-08 22:06:10 -0400

    HI! Use Select.RS to parse hackernews html and output the articles
    The eventual goal is to write a cli/gtk app (possibly) for interacting
    with hacker news. Mostly though this is just for learning and comming to
    terms with Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+extern crate hyper;
+extern crate select;
+
+use select::document::Document;
+use select::predicate::{Class, Name};
+use select::node::Node;
+
+struct Article {
+    title: String,
+    link: String,
+}
+
+impl Article {
+
+}
+
 fn main() {
-    println!("Hello, world!");
+    let hackernews = open_testing();
+    let document = Document::from(hackernews);
+    //println!("{}", hackernews);
+
+    for node in document.find(Class("storylink")) {
+        println!("{} ({:?})", node.text(), node.attr("href").unwrap());
+        println!("---")
+    }
+
+}
+
+fn open_testing() -> &'static str {
+    // include_str! is a macro that will read in a file as a string
+    include_str!("hacker-news.html")
 }
